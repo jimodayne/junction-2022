22d7f6ed98cd1f05dbfc3938a697f046fbf35348
Thinh Tran <tio212@gmail.com>
2022-11-05 19:04:30 +0200

    feat: add prettier

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,29 +1,32 @@
-import Link from 'next/link';
-import { IPost } from 'src/service/PostService';
-import styles from './Post.module.css';
+import Link from 'next/link'
+import { IPost } from 'src/service/PostService'
+import styles from './Post.module.css'
+import dayjs from 'dayjs'
 
 interface PostCardProps extends IPost {}
 const Post = (props: PostCardProps) => {
-  const { id, title, description, createdAt, category } = props;
+    const { id, title, description, createdAt, category } = props
 
-  return (
-    // <Link key={id} href={`/${id}`}>
-    <div className={styles.postContainer}>
-      <div className={styles.postLeft}>
-        <h4 className={styles.postCategory}>{category}</h4>
-        <h2 className={styles.postTitle}>{title}</h2>
-        <p className={styles.postDesc}>{description}</p>
-        <p className={styles.postDate}>{createdAt}</p>
-      </div>
-      <div className={styles.postRight}>
-        <img
-          src='https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-          alt='sport'
-          className={styles.postImg}
-        />
-      </div>
-    </div>
-  );
-};
+    return (
+        // <Link key={id} href={`/${id}`}>
+        <div className={styles.postContainer}>
+            <div className={styles.postLeft}>
+                <h4 className={`${styles.postCategory}  underline decoration-primary`}>
+                    {category}
+                </h4>
+                <h2 className={styles.postTitle}>{title}</h2>
+                <p className={styles.postDesc}>{description}</p>
+                <p className={styles.postDate}>{dayjs(createdAt).format('DD MMM')}</p>
+            </div>
+            <div className={styles.postRight}>
+                <img
+                    src="https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                    alt="sport"
+                    className={styles.postImg}
+                />
+            </div>
+        </div>
+    )
+}
 
-export default Post;
+export default Post
--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,27 +1,27 @@
-import { IPost } from 'src/service/PostService';
-import Post from '../Post';
-import PostCard from '../PostCard';
-import styles from './PostList.module.css';
+import { IPost } from 'src/service/PostService'
+import Post from '../Post'
+import PostCard from '../PostCard'
+import styles from './PostList.module.css'
 
 interface PostListProps {
-  posts: IPost[];
+    posts: IPost[]
 }
 
 const PostList = (props: PostListProps) => {
-  const { posts } = props;
-  if (!posts?.length) return null;
+    const { posts } = props
+    if (!posts?.length) return null
 
-  return (
-    <div className={styles.postList}>
-      {posts.slice(0, 2)?.map((post) => (
-        <PostCard key={post.id} {...post} />
-      ))}
+    return (
+        <div className={styles.postList}>
+            {posts.slice(0, 2)?.map(post => (
+                <PostCard key={post.id} {...post} />
+            ))}
 
-      {posts.slice(3, posts.length)?.map((post) => (
-        <Post key={post.id} {...post} />
-      ))}
-    </div>
-  );
-};
+            {posts.slice(3, posts.length)?.map(post => (
+                <Post key={post.id} {...post} />
+            ))}
+        </div>
+    )
+}
 
-export default PostList;
+export default PostList
